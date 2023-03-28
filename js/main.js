4df767197eafd630b3de0ba5b9c0127f83535b91
Mauricio <maurolores1992@gmail.com>
2023-03-28 03:59:13 -0300

    agregando slider

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let slideIndex = 0;
+
+// funcion para el slider
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if(slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides,6000);
+}
+
+showSlides();
+
